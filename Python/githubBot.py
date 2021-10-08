8c57e9d7eb4b0fc94fc0a8188bc3f91a9eb20691
reda-coder <redaelouahabi98@gmail.com>
2021-10-08 19:41:07 +0100

    8Day❤

--- Python/githubBot.py
+++ Python/githubBot.py
@@ -1,3 +1,37 @@
+# Automated code
+#######################################################################
+# # Push to github
+# import os
+# def new_func(day):
+#     CommandCommit = "git commit -m " +str(str(day)+"Day❤") 
+#     os.system("git add .")
+#     os.system(CommandCommit)
+#     os.system("git push")
+
+# # Evry Day
+# import datetime 
+# from win10toast import ToastNotifier
+# def new_func1():
+#     day= datetime.datetime.today().day
+#     f = open("dateToday.txt","r")
+#     daySaved = f.readlines()
+#     f.close
+#     if(int(daySaved[0])==day) :
+#         pass
+#     else:
+#         new_func(day)
+#         notifier = ToastNotifier()
+#         notifier.show_toast("Khay Reda , I pushed the commit to github 😉 ",duration=30)
+#     f = open("dateToday.txt","w")
+#     f.write(str(day))
+#     f.close
+# import time
+
+# while(True):
+#     new_func1()
+#     time.sleep(60*30)
+#     continue
+#######################################################################
 
 # Push to github
 import os
