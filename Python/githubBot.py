cc55cd6d462e3c47ffc69281ab91a23d6ae3edd5
reda-coder <redaelouahabi98@gmail.com>
2021-10-08 19:16:51 +0100

    8

---
+++ Python/githubBot.py
@@ -0,0 +1,25 @@
+
+# Evry Day
+import datetime 
+day= datetime.datetime.today().day
+# print(day)
+
+f = open("dateToday.txt","r")
+daySaved = f.readlines()
+# print (daySaved)
+f.close
+if(int(daySaved[0])==day) :
+    print("Yes")
+else:
+    print("No")
+f = open("dateToday.txt","w")
+f.write(str(day))
+f.close
+
+# Push
+
+import os
+CommandCommit = "git commit -m "+str(day)
+os.system("git add .")
+os.system(CommandCommit)
+
